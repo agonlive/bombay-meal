aa8d3c19f95bc9e347bfe5b71bc1df73687a4a1e
Pobthorn <agonlive@gmail.com>
2023-10-06 22:39:14 +0700

    upgrade code

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,175 +1,175 @@
 import React, { useState, useRef } from "react";
-import { Card, LoadingOverlay, Image, Text, Badge, Button, Group, Textarea, FileInput, Radio, CheckIcon } from '@mantine/core';
+import { Card, LoadingOverlay, Text, Badge, Button, Textarea, Radio, CheckIcon } from '@mantine/core';
 import { DateInput } from '@mantine/dates';
 import { IKContext, IKImage, IKUpload } from 'imagekitio-react';
-import axios, { AxiosError } from "axios";
-
-
-
+import axios from "axios";
 
 export default function Form() {
+  const sheetUrl = 'https://sheetdb.io/api/v1/lrlbxjz2wqd1l';
+  const imgBBKey = '09de3fb63bf4601a35eccb68ddba7ef3';
 
-  const sheetUrl = 'https://sheetdb.io/api/v1/lrlbxjz2wqd1l'
-  const imgBBKey = `09de3fb63bf4601a35eccb68ddba7ef3`
-  const [date, setDate] = useState(new Date())
-  const [des, setDes] = useState('')
-  const [image, setImage] = useState(null)
-  const [goodMeal, setGoodMeal] = useState('Yes')
-  const [loading, setLoading] = useState(false)
-  const [base64String, setBase64String] = useState(null)
-  const inputRef = useRef(null)
+  const [formValues, setFormValues] = useState({
+    date: new Date(),
+    des: '',
+    image: null,
+    goodMeal: 'Yes',
+  });
 
-  const onClickSave = async (e) => {
-    setLoading(true)
-    const today = date
-    const formattedDate = formatDateToDDMMYYYY(today);
-    let respondImage
-    let base64String
-    const temp = {
-      no: '+ROW()-1',
-      date: formattedDate,
-      des: des,
-      picture: image,
-      good_meal: goodMeal,
-    }
+  const [loading, setLoading] = useState(false);
 
-    console.log('file', inputRef.current.files[0]);
-    // const reader = new FileReader();
-    // reader.onload = (e) => {
-    //   const base64 = e.target.result.split('data:image/png;base64,')[1] ?? '';
-    //   // setBase64String(base64); // Set the Base64 string
-    //   alert(base64)
-    // };
-    // if (base64String) {
-    const formData = new FormData();
-    formData.append('key', imgBBKey);
-    formData.append('image', inputRef.current.files[0]);
-    formData.append('expiration', 0)
-    respondImage = await axios.post(`https://api.imgbb.com/1/upload`, formData)
-    console.log('respond', respondImage.data.data.display_url);
-    temp.picture = respondImage.data.data.display_url
+  const inputRef = useRef(null);
 
-    // }
-
-
-    const res = await axios.post(sheetUrl, temp)
-
-    setLoading(false)
-    alert('เซฟข้อมูลเรียบร้อยแล้ว')
-  }
-
-  function formatDateToDDMMYYYY(date) {
+  const formatDateToDDMMYYYY = (date) => {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const year = date.getFullYear();
-
     return `${day}/${month}/${year}`;
-  }
-
+  };
 
   const handleImageChange = (e) => {
-    console.log('e ', e);
-    alert('hi ')
-
     const file = e.target.files[0];
-    // alert('ok')
-
     if (file) {
       const reader = new FileReader();
-      alert('ok')
       reader.onload = (e) => {
         const base64 = e.target.result.split('data:image/png;base64,')[1] ?? '';
-        setBase64String(base64); // Set the Base64 string
-        alert(base64)
+        setFormValues({
+          ...formValues,
+          image: base64,
+        });
       };
-
       reader.readAsDataURL(file);
-      console.log('file', file);
-
     } else {
-      // Handle the case when no file is selected
-      // setPreviewSrc('');
-      // setBase64String('');
+      setFormValues({
+        ...formValues,
+        image: null,
+      });
     }
   };
 
-  const onChangeDate = (e) => {
-    setDate(e)
-  }
+  const onChangeDate = (date) => {
+    setFormValues({
+      ...formValues,
+      date,
+    });
+  };
 
+  const onClickSave = async () => {
+    setLoading(true);
+    const formattedDate = formatDateToDDMMYYYY(formValues.date);
 
-  return <div className="">
-    <Card shadow="sm" padding="lg" radius="md" withBorder className="p-5">
-      <LoadingOverlay
-        visible={loading}
-        zIndex={1000}
-        overlayProps={{ radius: 'sm', blur: 2 }}
-        loaderProps={{ color: 'pink', type: 'bars' }}
-      />
-      <Card.Section>
-        <Text
-          className="py-4"
-          size="xl"
-          fw={900}
-          variant="gradient"
-          gradient={{ from: 'gray', to: 'rgba(84, 84, 84, 1)', deg: 90 }}> เพิ่มรายการอาหาร 🍳 </Text>
-      </Card.Section>
+    try {
+      // Upload image to imgBB
+      const formData = new FormData();
+      formData.append('key', imgBBKey);
+      formData.append('image', inputRef.current.files[0]);
+      formData.append('expiration', 0);
+      const respondImage = await axios.post(`https://api.imgbb.com/1/upload`, formData);
 
-      <div className="flex flex-col gap-3">
+      // Save data to the sheet
+      const dataToSave = {
+        no: '+ROW()-1',
+        date: formattedDate,
+        des: formValues.des,
+        picture: respondImage.data.data.display_url,
+        good_meal: formValues.goodMeal,
+      };
 
-        <div>
-          <DateInput
+      await axios.post(sheetUrl, dataToSave);
 
-            value={date}
-            onChange={onChangeDate}
-            placeholder="วันที่ลงภาพ"
-          />
-        </div>
-        <div>
-          <Textarea
-            value={des}
-            onChange={(e) => setDes(e.target.value)}
-            placeholder="อธิบายคราวๆ อาหาร"
-          />
-        </div>
-        {/* <Button
-          className="bg-[#099268] text-[#fff] border-0"
-          variant="gradient"
-          gradient={{ from: 'gray', to: 'gray', deg: 90 }}
-          onClick={() => inputRef.current.click()}
-        >
-          Upload
-        </Button> */}
+      setFormValues({
+        date: new Date(),
+        des: '',
+        image: null,
+        goodMeal: 'Yes',
+      });
 
+      setLoading(false);
+      alert('เซฟข้อมูลเรียบร้อยแล้ว');
+    } catch (error) {
+      console.error('Error:', error);
+      alert('เช็คการอัพรูปด้วย')
+      setLoading(false);
+    }
+  };
 
-        {/* <FileInput
-          label="Input label"
-          description="Input description"
-          placeholder="Input placeholder"
-          onChange={handleImageChange}
-        /> */}
-
-        <input
-          // onInput={handleImageChange}
-          name="inputFile"
-          className=""
-          ref={inputRef}
-          id="fileInput"
-          type="file"
-          accept="image/*"
-        // onChange={handleImageChange}
+  return (
+    <div className="">
+      <Card shadow="sm" padding="lg" radius="md" withBorder className="p-5">
+        <LoadingOverlay
+          visible={loading}
+          zIndex={1000}
+          overlayProps={{ radius: 'sm', blur: 2 }}
+          loaderProps={{ color: 'pink', type: 'bars' }}
         />
+        <Card.Section>
+          <Text
+            className="py-4"
+            size="xl"
+            fw={900}
+            variant="gradient"
+            gradient={{ from: 'gray', to: 'rgba(84, 84, 84, 1)', deg: 90 }}
+          >
+            เพิ่มรายการอาหาร 🍳
+          </Text>
+        </Card.Section>
 
-        <div className="flex gap-2">
-          <Radio icon={CheckIcon} label="✅ Good meal" name="check" value="Yes" defaultChecked onChange={(e) => setGoodMeal(e.target.value)} />
-          <Radio icon={CheckIcon} label="👎🏻 Bad meal" name="check" value="No" onChange={(e) => setGoodMeal(e.target.value)} />
+        <div className="flex flex-col gap-3">
+          <div>
+            <DateInput
+              value={formValues.date}
+              onChange={onChangeDate}
+              placeholder="วันที่ลงภาพ"
+            />
+          </div>
+          <div>
+            <Textarea
+              value={formValues.des}
+              onChange={(e) => setFormValues({ ...formValues, des: e.target.value })}
+              placeholder="อธิบายคราวๆ อาหาร"
+            />
+          </div>
+
+          <input
+            name="inputFile"
+            ref={inputRef}
+            id="fileInput"
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+
+          <div className="flex gap-2">
+            <Radio
+              icon={CheckIcon}
+              label="✅ Good meal"
+              name="check"
+              value="Yes"
+              defaultChecked={formValues.goodMeal === 'Yes'}
+              onChange={(e) => setFormValues({ ...formValues, goodMeal: e.target.value })}
+            />
+            <Radio
+              icon={CheckIcon}
+              label="👎🏻 Bad meal"
+              name="check"
+              value="No"
+              defaultChecked={formValues.goodMeal === 'No'}
+              onChange={(e) => setFormValues({ ...formValues, goodMeal: e.target.value })}
+            />
+          </div>
+
+          <Button
+            onClick={onClickSave}
+            color="violet"
+            className="bg-[#228be6] text-[#fff] border-0"
+            justify="center"
+            fullWidth
+            variant="default"
+            mt="md"
+          >
+            ⭐️ Save
+          </Button>
         </div>
-
-        <Button onClick={onClickSave} color="violet" className="bg-[#228be6] text-[#fff] border-0" justify="center" fullWidth variant="default" mt="md">
-          ⭐️ Save
-        </Button>
-
-      </div>
-    </Card>
-  </div>;
+      </Card>
+    </div>
+  );
 }
